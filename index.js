69e028b62f5351864285dcb776c1c5c0efc1df28
式燕 <wenjian.ywj@alibaba-inc.com>
2016-11-11 00:20:58 +0800

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 import inherit from './lib/inherit'
 import { get, set } from './lib/cache'
 import { transform, print, createElement, resolve as re } from './lib/manipulate'
-
-function Seed() {
-  // prepare some context
-  if (this.prepare) this.prepare()
-}
-// state, props, children
-Seed.prototype.render = function render(state, props, children) {
-  return this.tpl(state, props, children)
-}
+import Seed from './lib/seed'
 
 export const create = (name, prototype, staticProps, Super) => {
   let Child = get(name)
