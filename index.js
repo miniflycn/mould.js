974817bccdff85e7f371c2861caddb72c2e1a62b
式燕 <wenjian.ywj@alibaba-inc.com>
2016-11-24 14:26:19 +0800

    add aop support

--- index.js
+++ index.js
@@ -20,7 +20,27 @@ const set = (key, value) => {
   _[key] = value;
 };
 
-const get = (key) => _[key];
+const get$1 = (key) => _[key];
+
+const before = function before(fn, be) {
+  return function caller(...args) {
+    if (be.apply(this, args) === false) {
+      return false
+    }
+    return fn.apply(this, args)
+  }
+};
+
+const after = function after(fn, af) {
+  return function caller(...args) {
+    const ret = fn.apply(this, args);
+    if (ret === false) return false
+    af.apply(this, args);
+    return ret
+  }
+};
+
+const aop = { before, after };
 
 /**
  * {
@@ -62,7 +82,7 @@ function solve(type, Origin) {
  */
 const createElement = (Element, props, ...args) => {
   args.unshift(Element, props);
-  if (Element.__Origin__ && get(Element.__Origin__.__name__)) {  // eslint-disable-line
+  if (Element.__Origin__ && get$1(Element.__Origin__.__name__)) {  // eslint-disable-line
     const fn = solve('createElement', Element.__Origin__); // eslint-disable-line
     if (fn) return fn.apply(null, args)
   }
@@ -84,7 +104,7 @@ function trans(ele) {
   const children = ele.children;
   const props = Object.assign({}, ele.props, ele.key != null ? { key: ele.key } : null);
   const args = [
-    get(ele.tagname) || ele.tagname,
+    get$1(ele.tagname) || ele.tagname,
     props,
   ];
   if (children && children.length) {
@@ -102,14 +122,39 @@ const transform = (Origin) => {
   if (fn) {
     res = fn(Origin);
   } else {
+    const origin = new Origin;
     const param = {
       getInitialState() { return Origin.state || {} },
       render: function render() {
-        const origin = this.origin || new Origin(this);
+        // run init
+        if (origin.element !== this) origin.init(this);
         const dom = origin.render(this.state, this.props, this.children);
         return React.isValidElement(dom) ? dom : trans(dom)
       },
     };
+    if (origin.bindEvent) param.componentDidMount = origin.bindEvent.bind(origin);
+    if (origin.aop && origin.aop['after:bindEvent'] && !param.componentDidMount) {
+      param.componentDidMount = function bindEvent() {};
+    }
+    if (origin.aop) {
+      Object.keys(origin.aop).map(key => {
+        const ret = key.split(':');
+        ret.push(origin.aop[key]);
+        return ret
+      }).forEach(item => {
+        const foo = aop[item[0]];
+        switch (item[1]) {
+          case 'render':
+            param.render = foo(param.render, item[2]);
+            break
+          case 'bindEvent':
+            param.componentDidMount = foo(param.componentDidMount, item[2]);
+            break
+          default:
+            break
+        }
+      });
+    }
     res = React.createClass(param);
   }
   res.__Origin__ = Origin; // eslint-disable-line
@@ -128,9 +173,7 @@ const resolve$1 = (config) => {
   resolves.push(config);
 };
 
-function Seed(element) {
-  this.element = element;
-  element.origin = this; // eslint-disable-line
+function Seed() {
   // prepare some context
   if (this.prepare) this.prepare();
 }
@@ -138,14 +181,17 @@ function Seed(element) {
 Seed.prototype.render = function render(state, props, children) {
   return this.tpl(state, props, children)
 };
+Seed.prototype.init = function init(element) {
+  this.element = element;
+};
 
 const create = (name, prototype, staticProps, Super) => {
-  let Child = get(name);
+  let Child = get$1(name);
   if (Child == null) {
     /* eslint-disable */
     Super = Super ? Super.__Origin__ : Seed;
-    const Origin = function (element) {
-      Super.call(this, element);
+    const Origin = function () {
+      Super.call(this);
     };
     inherit(Origin, Super);
     Object.assign(Origin, Super, staticProps);
@@ -160,6 +206,10 @@ const create = (name, prototype, staticProps, Super) => {
   return Child
 };
 
+const get$$1 = get$1;
+
+var index = { createElement };
+
 const render$1 = (container, Element, props) => {
   const element = createElement(Element, props);
   print(container, element);
@@ -171,5 +221,7 @@ const resolve$$1 = (config) => {
 };
 
 exports.create = create;
+exports.get = get$$1;
+exports['default'] = index;
 exports.render = render$1;
 exports.resolve = resolve$$1;
